6a74333e07cea5c24a166408a49c40d90191c53b
santiagoibazan <santiagoibazan@gmail.com>
2026-05-19 14:51:56 -0300

    Ejercicio 1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Num");
+int a;
+
+if (int.TryParse(Console.ReadLine(),out a) && a > 0) {
+    Console.WriteLine("Numero ingresado: " + a);
+    int aux = 0;
+    while (a >= 10) {
+        aux = aux + (a % 10);
+        aux *= 10;
+        a = a / 10;
+    }
+    aux = aux + a;
+    Console.WriteLine("Numero invertido: " + aux);
+} else {
+    Console.WriteLine("Error");
+}
+
+
+
